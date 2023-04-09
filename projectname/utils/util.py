6ceb8f4304ad6abe681e064f0a9f32385936f209
SNandini04 <nandani-4-8@gmail.com>
2023-04-09 13:01:02 +0530

    all file added

---
+++ projectname/utils/util.py
@@ -0,0 +1,1 @@
+#model_training: save the model or i nedd to load the model 
---
+++ setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup, find_packages
+
+setup(name="projectname",version="0.0.1",author = "Nandini",
+author_email="nandani0408@gmail.com",
+packages=find_packages(),install_requires=['pandas','numpy','flask'])
+
+
